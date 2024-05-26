dae517feabd22d907ba48327c90df7832b975c2a
jfkwebdev <jfkwebdev@gmail.com>
2024-05-26 16:49:56 -0500

    module work around

--- src/app/api/stationthisdeluxe/routes.tsx
+++ src/app/api/stationthisdeluxe/routes.tsx
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 import { getBotInstance } from '@/deluxebot/utils/bot/bot.js';
-const watch = require('@/deluxebot/utils/bot/handlers/watch.js');
-const wait = require('@/deluxebot/utils/bot/handlers/menu.js');
 
 const bot = getBotInstance();
 
