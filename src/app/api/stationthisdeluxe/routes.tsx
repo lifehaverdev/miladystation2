832dd1539839be091dd6f28172cb327191eb68ed
jfkwebdev <jfkwebdev@gmail.com>
2024-05-26 20:10:57 -0500

    routes logging

--- src/app/api/stationthisdeluxe/routes.tsx
+++ src/app/api/stationthisdeluxe/routes.tsx
@@ -5,7 +5,18 @@ import { getBotInstance } from '@/deluxebot/utils/bot/bot.js';
 const bot = getBotInstance();
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  bot.processUpdate(body);
-  return NextResponse.json({ status: 'ok' });
+  try {
+    console.log('Webhook received');
+    
+    const body = await request.json();
+    console.log('Request body:', body);
+
+    // Process the update with the bot instance
+    await bot.processUpdate(body);
+
+    return NextResponse.json({ status: 'ok' });
+  } catch (error:any) {
+    console.error('Error processing webhook:', error);
+    return NextResponse.json({ status: 'error', message: error.message });
+  }
 }
